a5670713ec5467d590bb11e433fcef335a0dc64f
Jovan Veljanoski <jovan.veljanoski@tiqets.com>
2023-03-21 00:37:37 +0100

    api key setting is now responsive to user changes

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,80 @@ import { ButtonExtension } from './button';
 /**
  * Initialization data for the voicepilot extension.
  */
+const PLUGIN_ID = 'voicepilot:plugin';
 const plugin: JupyterFrontEndPlugin<void> = {
   // activate,
-  id: 'voicepilot:plugin',
+  id: PLUGIN_ID,
   autoStart: true,
   requires: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  activate: (app: JupyterFrontEnd, settings: ISettingRegistry | null) => {
+    const { commands } = app;
     app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
     console.log('JupyterLab extension voicepilot is activated!');
 
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('voicepilot settings loaded:', settings.composite);
-          const key = settings.get('open_api_key').composite;
-          console.log('mything:', key);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for voicepilot.', reason);
+    let apiKey = '';
+
+    /**
+     * Load the settings for this extension
+     *
+     * @param setting Extension settings
+     */
+    function loadSetting(setting: ISettingRegistry.ISettings): void {
+      apiKey = setting.get('open_api_key').composite as string;
+      console.log('apiKey:', apiKey);
+    };
+
+    // Wait for the application to be restored and
+    // for the settings for this plugin to be loaded
+    Promise.all([app.restored, settings?.load(PLUGIN_ID)])
+      .then(([, setting]) => {
+        // Read the settings
+        loadSetting(setting!);
+
+        // Listen for your plugin setting changes using Signal
+        setting?.changed.connect(loadSetting);
+
+        commands.addCommand('COMMAND_ID', {
+          label: 'Modify API Key',
+          // isToggled: () => apiKey,
+          execute: () => {
+            // Programmatically change a setting
+            Promise.all([
+              setting?.set('open_api_key', apiKey),
+            ])
+              .then(() => {
+                const newKey = setting?.get('open_api_key').composite as number;
+                window.alert(`VoicePilot: API is set to '${newKey}'.`);
+              })
+              .catch((reason) => {
+                console.error(
+                  `Something went wrong when changing the settings.\n${reason}`
+                );
+              });
+          },
         });
-    } else {
-      console.warn('Setting registry not available.');
-    }
+      })
+      .catch((reason) => {
+        console.error(
+          `Something went wrong when reading the settings.\n${reason}`
+        );
+      });
+
+
+    // if (settingRegistry) {
+    //   settingRegistry
+    //     .load(plugin.id)
+    //     .then(settings => {
+    //       console.log('voicepilot settings loaded:', settings.composite);
+    //       const key = settings.get('open_api_key').composite;
+    //       console.log('mything:', key);
+    //     })
+    //     .catch(reason => {
+    //       console.error('Failed to load settings for voicepilot.', reason);
+    //     });
+    // } else {
+    //   console.warn('Setting registry not available.');
+    // }
 
   }
 };
